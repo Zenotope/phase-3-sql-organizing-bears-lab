9382f259c8728789987ff68a910c2fd6cfa3e2ea
Zenotope <zaak.kerstetter@gmail.com>
2022-06-30 10:42:54 -0600

    commit tree c980ac3501a63f08c6c67ca19ceff1844a3e537c

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES 
+(1,"Mr. Chocolate", 3, "M","brown","nice",1),
+(2, "Rowdy", 3, "M","brown","nice",1),
+(3, "Tabitha", 3, "M","brown","nice",1),
+(4, "Sergeant Brown", 3, "M","brown","nice",1),
+(5, "Melissa", 3, "M","brown","nice",0),
+(6, "Grinch", 3, "M","brown","nice",1),
+(7, "Wendy", 3, "M","brown","nice",1),
+(8, null, 3, "M","brown","nice",1);
